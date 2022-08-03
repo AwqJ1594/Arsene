57db9e5962bbb95363b274d419159c6e7bc62b17
Awq_Jerry <63896764+AwqJ1594@users.noreply.github.com>
2022-08-04 02:11:06 +0800

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -17,3 +17,46 @@ function Alert_Show(numa){
             alert("嘿！七夕节快乐！"+"\n"+"还有，你已经在彩蛋大厅里了，你见过原地TP吗？(。>︿<)_θ");
     }
 }
+
+function ScreenWidth() {      //获取屏幕尺寸，判断PC端或移动端
+    if (/phone|pad|pod|iPhone|iPod|ios|iPad|Android|Fennec|BlackBerry|Mobile|IEMobile|MQQBrowser|JUC|Fennec|WosBrowser|BrowserNG|WebOS|Symbian|Windows Phone/i.test(navigator.userAgent)) {
+//如果是移动端，则跳转到移动端对应的页面
+          window.location.href = 'aknowlege.html';
+      }
+  }
+function windowAddMouseWheel() {
+    var scrollFunc = function (e) {
+        e = e || window.event;
+    if (e.wheelDelta) {  //判断浏览器IE，谷歌滑轮事件
+        // if (e.wheelDelta > 0) { //当滑轮向上滚动时
+        //     alert("滑轮向下滚动");
+        // }
+        if (e.wheelDelta < 30) { //当滑轮向下滚动时
+            var py1 = document.getElementById("play1");	//歌曲1
+            var music_arr = [py1];
+            var index = 0;		//数组下标
+            music_arr[index].play();
+        }
+    } else if (e.detail) {  //Firefox滑轮事件
+        // if (e.detail> 0) { //当滑轮向上滚动时
+        //     alert("滑轮向下滚动");
+        // }
+        if (e.detail< 15) { //当滑轮向下滚动时
+            var py1 = document.getElementById("play1");	//歌曲1
+            var music_arr = [py1];
+            var index = 0;		//数组下标
+            music_arr[index].play();
+        }
+    }
+}
+}
+
+function a(){
+    var audio = document.getElementById('music'); 
+    if(audio.paused){                 
+        audio.play();//audio.play();// 播放  
+    }
+    else{
+         audio();
+    } 
+  }
