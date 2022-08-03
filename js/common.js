45a769692294e2f205a3f3cbfd8103ceb28c8d97
Awq_Jerry <63896764+AwqJ1594@users.noreply.github.com>
2022-08-03 10:46:11 +0800

    V0.01
    Something-new

---
+++ js/common.js
@@ -0,0 +1,19 @@
+function Jump_To_Egg(){
+    window.location.href="egg01.html";
+}
+function jump_to_help(){
+    window.location.href="help.html";
+}
+function go_to_help(){
+    window.location.herf="help.html";
+}
+function Alert_Show(numa){
+    switch(numa){
+        case 1:
+            alert("嘿！七夕节快乐！"+"\n"+"还有，你已经在大厅里了，你见过原地TP吗？(。>︿<)_θ");
+         case 2:
+            alert("嘿！七夕节快乐！"+"\n"+"还有，你已经在帮助页面里了，你见过原地TP吗？(。>︿<)_θ"); 
+        case 3:
+            alert("嘿！七夕节快乐！"+"\n"+"还有，你已经在彩蛋大厅里了，你见过原地TP吗？(。>︿<)_θ");
+    }
+}
